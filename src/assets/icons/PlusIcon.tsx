b91502ff69a77b53eb218a70d3048d260c508d8c
behnam <salehinezhad.b@gmail.com>
2022-07-19 05:17:33 +0430

    add icons

---
+++ src/assets/icons/PlusIcon.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  className: string;
+}
+
+export function PlusIcon(props: Props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={props.className}
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+    </svg>
+  );
+}
