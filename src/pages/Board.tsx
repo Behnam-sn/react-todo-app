3604069083fb3e5cae38a4753e06a1efad712b91
behnam <salehinezhad.b@gmail.com>
2022-07-19 05:21:04 +0430

    update pages

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,7 +1,68 @@
-import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { boardUpdated } from "../features/board/boardSlice";
+
+import { List } from "../features/list/List";
+import { AddListButton } from "../features/list/AddListButton";
+import { EditableTextBox } from "../components/EditableTextBox";
+
+import { LeftArrowIcon } from "../assets/icons/LeftArrowIcon";
+
+import type { RootState } from "../app/store";
 
 export function Board() {
   let params = useParams();
-  // let invoice = getInvoice(parseInt(params.invoiceId, 10));
-  return <div>Board: {params.boardId}</div>;
+  const dispatch = useDispatch();
+  const boards = useSelector((state: RootState) => state.boards);
+  const lists = useSelector((state: RootState) => state.lists);
+
+  const currentBoard = boards.find((board) => board.id === params.boardId);
+
+  useEffect(() => {
+    if (currentBoard) {
+      document.body.classList.value = currentBoard.color;
+    }
+  });
+
+  if (currentBoard) {
+    const filteredLists = lists.filter((list) => {
+      return list.boardId === currentBoard.id;
+    });
+
+    const updateBoard = (title: string) => {
+      dispatch(boardUpdated({ id: currentBoard.id, title: title }));
+    };
+
+    return (
+      <div className="mx-8">
+        <div className="mt-4 mb-8 flex items-center">
+          <Link to="/" className="mr-4">
+            <LeftArrowIcon className="w-10 text-white" />
+          </Link>
+          <div className="h-12 text-3xl font-medium text-white">
+            <EditableTextBox
+              text={currentBoard.title}
+              updateFunction={updateBoard}
+            />
+          </div>
+        </div>
+        <div className="flex">
+          <div className="flex flex-row">
+            {filteredLists.map((list) => (
+              <List
+                key={list.id}
+                id={list.id}
+                title={list.title}
+                boardId={list.boardId}
+              />
+            ))}
+            <AddListButton boardId={currentBoard.id} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <div>Board Not Found :(</div>;
+  }
 }
--- src/pages/Boards.tsx
+++ src/pages/Boards.tsx
@@ -1,3 +1,32 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import { Board } from "../features/board/Board";
+import { AddBoardButton } from "../features/board/AddBoardButton";
+
+import type { RootState } from "../app/store";
+
 export function Boards() {
-  return <div>Boards</div>;
+  const boards = useSelector((state: RootState) => state.boards);
+
+  useEffect(() => {
+    document.body.classList.value = "bg-white";
+  });
+
+  return (
+    <div className="mx-8">
+      <div className="mt-4 mb-8 h-12 text-4xl font-bold">Boards</div>
+      <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-5">
+        {boards.map((board) => (
+          <Board
+            key={board.id}
+            id={board.id}
+            title={board.title}
+            color={board.color}
+          />
+        ))}
+        <AddBoardButton />
+      </div>
+    </div>
+  );
 }
